4144a019fcd9f2f901a23f44eb79532006505787
Fang520 <76530785@qq.com>
2014-04-15 18:08:18 +0800

    modify

--- jpeg_otimizer.c
+++ jpeg_otimizer.c
@@ -28,7 +28,7 @@ static int build_dec_vlc(jpeg_ctx_t *ctx)
 	return 0;
 }
 
-int build_enc_vlc(jpeg_ctx_t *ctx)
+static int build_enc_vlc(jpeg_ctx_t *ctx)
 {
 	make_canonical_huffman_codes(ctx->dht_dc_hash[0].size, ctx->dht_dc_hash[0].code, ctx->dht_dc[0].len, ctx->dht_dc[0].val);
 	make_canonical_huffman_codes(ctx->dht_dc_hash[1].size, ctx->dht_dc_hash[1].code, ctx->dht_dc[1].len, ctx->dht_dc[1].val);
@@ -52,11 +52,71 @@ static void free_ctx(jpeg_ctx_t *ctx)
 	free(ctx);
 }
 
+static int init_dec_bit_stream(jpeg_ctx_t *ctx, uint8_t *buf, int len)
+{
+	const uint8_t *src = buf_ptr;
+	uint8_t *dst = s->buffer;
+
+	while (src < buf_end)
+	{
+		uint8_t x = *(src++);
+		*(dst++) = x;
+			if (x == 0xff) 
+			{
+				while (src < buf_end && x == 0xff)
+					x = *(src++);
+				if (x >= 0xd0 && x <= 0xd7)
+					*(dst++) = x;
+				else if (x)
+					break;
+			}
+	}
+
+	init_get_bits(&ctx->dec_bit_ctx, buf, len * 8);
+
+	return 0;
+}
+
+static int init_enc_bit_stream(jpeg_ctx_t *ctx, uint8_t *buf, int len)
+{
+	init_put_bits(&ctx->enc_bit_ctx, buf, len * 8);
+}
+
+
+static int decode_dc(jpeg_ctx_t *ctx, int yuv_index, uint16_t mb[])
+{
+	return 0;
+}
+
+static int decode_ac(jpeg_ctx_t *ctx, int yuv_index, uint16_t mb[])
+{
+	return 0;
+}
+
+static int encode_dc(jpeg_ctx_t *ctx, int yuv_index, uint16_t mb[])
+{
+	return 0;
+}
+
+static int encode_ac(jpeg_ctx_t *ctx, int yuv_index, uint16_t mb[])
+{
+	return 0;
+}
+
+static int re_quantize(jpeg_ctx_t *ctx, uint16_t mb[])
+{
+
+}
+
 /* 0=ok -1=err 1=end*/
 static int process_mb(jpeg_ctx_t *ctx, int yuv_index)
 {
-
-
+	uint16_t mb[64];
+	decode_dc(ctx, yuv_index, mb);
+	decode_ac(ctx, yuv_index, mb);
+	re_quantize(ctx, mb);
+	encode_dc(ctx, yuv_index, mb);
+	encode_dc(ctx, yuv_index, mb);
 }
 
 /* 0=ok -1=err 1=end*/
@@ -67,7 +127,7 @@ static int process_mcu(jpeg_ctx_t *ctx)
 	{
 		for (j = 0; j < ctx->rate_h[i] * ctx->rate_v[i]; j++)
 		{
-			ret = process_mb(ctx, i);
+			ret = process_mb(ctx, i ? 1 : 0);
 			if (ret == 1)
 				return 0;
 			else if (ret == -1)
@@ -100,6 +160,11 @@ static int process_body(jpeg_ctx_t *ctx)
 	return 0;
 }
 
+static int add_tailer(jpeg_ctx_t *ctx)
+{
+	return 0;
+}
+
 int optimize_jpeg(const uint8_t *input, int input_len, uint8_t *output, int *output_len, int qscale)
 {
 	jpeg_ctx_t *ctx;
@@ -119,8 +184,13 @@ int optimize_jpeg(const uint8_t *input, int input_len, uint8_t *output, int *out
 		free_ctx(ctx);
 		return -1;
 	}
-	ctx->in_buf = input;
-	ctx->in_pos = input + ret;
+	
+	ret = init_dec_bit_stream(ctx, input + ret, input_len - ret);
+	if (ret < 0)
+	{
+		free_ctx(ctx);
+		return -1;
+	}
 
 	ret = build_opt_header(ctx, output, *output_len);
 	if (ret < 0)
@@ -128,8 +198,13 @@ int optimize_jpeg(const uint8_t *input, int input_len, uint8_t *output, int *out
 		free_ctx(ctx);
 		return -1;
 	}
-	ctx->out_buf = output;
-	ctx->out_pos = output + ret;
+	
+	ret = init_enc_bit_stream(ctx, output + ret, *output_len - ret);
+	if (ret < 0)
+	{
+		free_ctx(ctx);
+		return -1;
+	}
 
 	ret = build_enc_vlc(ctx);
 	if (ret != 0)
@@ -146,6 +221,13 @@ int optimize_jpeg(const uint8_t *input, int input_len, uint8_t *output, int *out
 	}
 
 	ret = process_body(ctx);
+	if (ret != 0)
+	{
+		free_ctx(ctx);
+		return -1;
+	}
+
+	ret = add_tailer(ctx);
 	if (ret != 0)
 	{
 		free_ctx(ctx);
